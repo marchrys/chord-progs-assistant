f8c3602e9620b78077b02667a3bf2e6bcafbb9b3
marchrys <mar.chrysikopoulos@gmail.com>
2020-12-24 18:17:57 +0900

    Added custom fonts and created navbar and accordion for page.

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,17 @@
 // App module
 let app = {
   init: function(){
-     
+     const langStr =  window.navigator.language;
+     let lang = '';
+
+     if(langStr.substr(0, 2) == 'fr'){
+            lang = 'fr';
+      }
+      else{
+          lang = 'en';
+      }
+
+      console.log(lang);
   }
 }
 
